f93d6e88ceb54e3d26d923c74be90004d8443b6e
sohee <01okok@naver.com>
2025-01-18 17:41:34 +0900

    팝업css수정중

--- portfolio/client/src/pages/Ppop.tsx
+++ portfolio/client/src/pages/Ppop.tsx
@@ -10,13 +10,14 @@ interface datatype {
 
 const Ppop: React.FC<datatype> = ({ projectview, onClose }) => {
     useEffect(() => {
-        if (projectview) {
+        if (projectview && projectview.length > 0) {
             document.body.style.overflow = "hidden";
         }
         return () => {
             document.body.style.overflow = "";
         };
     }, [projectview]);
+
     if (!projectview || projectview.length === 0) {
         return <div>데이터가 없습니다.</div>;
     }
@@ -35,44 +36,56 @@ const Ppop: React.FC<datatype> = ({ projectview, onClose }) => {
     // ];
 
     return (
-        <div className="projectpopwrap">
-            <div className="projectpop">
+        <>
+            <div className="projectpopwrap">
                 <div className="popclose" onClick={onClose}>
                     <img
                         src="/sohee-portfolio/img/icon/close.svg"
                         alt="close"
                     />
                 </div>
-                {projectview.map(project => (
-                    <header key={project.id}>
-                        <div className="subtitle1 black1">{project.title}</div>
-                        <div className="pimgwrap">
-                            <img src={project.img} alt="silla" />
-                        </div>
-                    </header>
-                ))}
-                <div className="deswrap flex">
-                    <aside>
-                        <p className="subtitle1">{projectinfo[0].title}</p>
-                        <p className="label1 gray4">{projectinfo[0].people}</p>
-                        <p className="label1 gray4">기간 : 24.12 ~ 24.01</p>
-                        <p className="label1 gray4">인원 : 5명</p>
-                    </aside>
-                    <main>
-                        {deslist.map((data, index) => (
-                            <div className="section" key={index}>
-                                <div className="ptitle gray5 subtitle1">
-                                    {data.title}
+                <div className="projectpop">
+                    <div className="inner">
+                        {projectview.map(project => (
+                            <header key={project.id}>
+                                <div className="subtitle1 black1">
+                                    {project.title}
                                 </div>
-                                <div className="pdes caption1">
-                                    {data.content}
+                                <div className="pimgwrap">
+                                    <img src={project.img} alt="silla" />
                                 </div>
-                            </div>
+                            </header>
                         ))}
-                    </main>
+                        <div className="deswrap flex">
+                            <aside>
+                                <p className="subtitle1">
+                                    {projectinfo[0].title}
+                                </p>
+                                <p className="label1 gray4">
+                                    {projectinfo[0].people}
+                                </p>
+                                <p className="label1 gray4">
+                                    기간 : 24.12 ~ 24.01
+                                </p>
+                                <p className="label1 gray4">인원 : 5명</p>
+                            </aside>
+                            <main>
+                                {deslist.map((data, index) => (
+                                    <div className="section" key={index}>
+                                        <div className="ptitle gray5 subtitle1">
+                                            {data.title}
+                                        </div>
+                                        <div className="pdes caption1">
+                                            {data.content}
+                                        </div>
+                                    </div>
+                                ))}
+                            </main>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
